c2ee964d87fa5b1ef478486149c5827a1c83d3d0
Lalita Awasthi <lily01awasthi@gmail.com>
2024-10-19 18:01:53 +0200

    cdk app initialized

---
+++ test/aws_data_suite.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsDataSuite from '../lib/aws_data_suite-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_data_suite-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsDataSuite.AwsDataSuiteStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
